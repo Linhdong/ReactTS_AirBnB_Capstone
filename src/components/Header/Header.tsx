66efb375150e1d3bb20c0dc3e7c619bf206f91a9
Duyen Nguyen <dnng1410@gmail.com>
2022-10-11 17:33:46 +0700

    header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
-type Props = {}
+const logo = require("../../assets/img/airbnb-logo.png");
+
+type Props = {};
 
 export default function Header({}: Props) {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="container py-4 d-flex justify-content-between align-items-center">
+        {/* left section - logo */}
+        <div className="header__logo">
+          <NavLink to="">
+            <img src={logo} alt="airbnb-logo" />
+          </NavLink>
+        </div>
+        {/* right section - logo */}
+
+        {/* middle section - search bar */}
+        <div className="header__search-bar">
+          <div className="search-bar d-flex align-items-center justify-content-between">
+            <input type="text" placeholder="Start your search" />
+            <button className="btnSearch">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+        {/* middle section - search bar */}
+
+        {/* right section */}
+        <div className="header__right d-flex">
+          <div className="become-host">
+            <button className="btnHost">Become a host</button>
+          </div>
+          <div className="language">
+            <button className="btnLanguage">
+              <i className="fas fa-globe"></i>
+            </button>
+          </div>
+          <div className="user-nav" data-toggle>
+            <button
+              className="btnUser"
+              onClick={() => setIsClicked(!isClicked)}
+            >
+              <i className="fa fa-bars"></i>
+              <i className="fa fa-user"></i>
+              <div className="notification d-inline-block">1</div>
+            </button>
+          </div>
+        </div>
+        <div id="user-menu" className={isClicked ? "d-block" : "d-none"}>
+          <div className="user-menu__top">
+            <ul>
+              <li>
+                <NavLink to="/signin">Log in</NavLink>
+              </li>
+              <li>
+                <NavLink to="/signup">Sign up</NavLink>
+              </li>
+            </ul>
+          </div>
+          <hr />
+          <div className="user-menu__bottom">
+            <ul>
+              <li>Host your home</li>
+              <li>Host an experience</li>
+              <li>Help</li>
+            </ul>
+          </div>
+        </div>
+        {/* right section */}
+      </div>
+    </header>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux';
-import {store} from './redux/configStore';
-import {createBrowserHistory} from 'history';
-import {unstable_HistoryRouter as HistoryRouter, Routes, Route, Navigate} from 'react-router-dom';
-import HomeTemplate from './templates/HomeTemplate';
-import Home from './pages/Home/Home';
-import SignIn from './pages/SignIn/SignIn';
-import SignUp from './pages/SignUp/SignUp';
-import Profile from './pages/Profile/Profile';
-import RoomList from './pages/RoomList/RoomList';
-import Detail from './pages/Detail/Detail';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/scss/style.scss";
+import { Provider } from "react-redux";
+import { store } from "./redux/configStore";
+import { createBrowserHistory } from "history";
+import {
+  unstable_HistoryRouter as HistoryRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import HomeTemplate from "./templates/HomeTemplate";
+import Home from "./pages/Home/Home";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import Profile from "./pages/Profile/Profile";
+import RoomList from "./pages/RoomList/RoomList";
+import Detail from "./pages/Detail/Detail";
 
 export const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
     <HistoryRouter history={history}>
       <Routes>
-        <Route path='' element={<HomeTemplate/>}>
-          <Route index element={<Home/>}></Route>
-          <Route path='signin' element={<SignIn/>}></Route>
-          <Route path='signup' element={<SignUp/>}></Route>
-          <Route path='profile' element={<Profile/>}></Route>
-          <Route path='roomlist'>
-            <Route path=':cityname' element={<RoomList/>}></Route>
+        <Route path="" element={<HomeTemplate />}>
+          <Route index element={<Home />}></Route>
+          <Route path="signin" element={<SignIn />}></Route>
+          <Route path="signup" element={<SignUp />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+          <Route path="roomlist">
+            <Route path=":cityname" element={<RoomList />}></Route>
           </Route>
-          <Route path='detail'>
-            <Route path=':id' element={<Detail/>}></Route>
+          <Route path="detail">
+            <Route path=":id" element={<Detail />}></Route>
           </Route>
         </Route>
-        <Route path='*' element={<Navigate to='' />}></Route>
+        <Route path="*" element={<Navigate to="" />}></Route>
       </Routes>
     </HistoryRouter>
   </Provider>
 );
-  
