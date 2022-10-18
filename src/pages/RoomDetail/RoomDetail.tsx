8e03d3068e13ef1d1f98628809c7ec79c89cf4d7
Duyen Nguyen <dnng1410@gmail.com>
2022-10-19 00:29:26 +0700

    roomDetails layout

--- src/pages/RoomDetail/RoomDetail.tsx
+++ src/pages/RoomDetail/RoomDetail.tsx
@@ -1,15 +1,37 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+import Comment from "../../components/Comment/Comment";
 import RoomBooking from "../../components/RoomBooking/RoomBooking";
 import RoomDetailHeading from "../../components/RoomDetailHeading/RoomDetailHeading";
 import { AppDispatch, RootState } from "../../redux/configStore";
-import { getRoomByIdApi } from "../../redux/reducers/roomReducer";
+import { getRoomByIdApi, Room } from "../../redux/reducers/roomReducer";
 
-// const houseIcon = require("../../assets/img/houseIcon.png");
-// const houseIcon = require("../../assets/img/houseIcon.svg");
+interface Amenities {
+  [key: string]: string;
+}
 
-import { ReactComponent as HouseIcon } from "../../houseIcon.svg";
+const amenitiesIcons: Amenities = {
+  banLa: "fas fa-tshirt",
+  bep: "fas fa-utensils",
+  doXe: "fas fa-parking",
+  hoBoi: "fas fa-swimming-pool",
+  mayGiat: "fas fa-tshirt",
+  tivi: "fas fa-tv",
+  wifi: "fas fa-wifi",
+  dieuHoa: "fas fa-wind",
+};
+
+const amenitiesNames: Amenities = {
+  banLa: "Bàn là",
+  bep: "Bếp",
+  dieuHoa: "Điều hoà",
+  doXe: "Chỗ đậu xe",
+  hoBoi: "Hồ bơi",
+  mayGiat: "Máy giặt",
+  tivi: "Tivi",
+  wifi: "Wifi",
+};
 
 type Props = {};
 
@@ -19,6 +41,18 @@ export default function RoomDetail({}: Props) {
   const { roomId } = useParams();
 
   const dispatch: AppDispatch = useDispatch();
+
+  const renderRoomAmenities = (amenity: boolean, key: string) =>
+    amenity ? (
+      <div className="room-amenity__item d-flex">
+        <div className="room-amenity__item--icon">
+          <i className={`${amenitiesIcons[key]} stroke-transparent`}></i>
+        </div>
+        <span className="ms-3">{amenitiesNames[key]}</span>
+      </div>
+    ) : (
+      ""
+    );
 
   useEffect(() => {
     dispatch(getRoomByIdApi(roomId));
@@ -33,7 +67,7 @@ export default function RoomDetail({}: Props) {
 
         {/* images */}
         <div className="room__images">
-          <img src="https://picsum.photos/1000" alt="..." />
+          <img src={room.hinhAnh} alt={room.tenPhong} />
           <div className="show-all__button">
             <button className="btnShowAll btn-bg-white btn-border-black">
               <i className="far fa-images me-2"></i>
@@ -49,23 +83,21 @@ export default function RoomDetail({}: Props) {
           <div className="room-info col-7">
             <div className="room-info__heading d-flex justify-content-between">
               <div className="room-info__heading--left w-70">
-                <h2>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                </h2>
+                <h2>{room.tenPhong}</h2>
                 <div className="room-info__stats d-flex">
-                  <span>2 khách</span>
+                  <span>{room.khach} khách</span>
                   <div className="dot">
                     <i className="fas fa-circle"></i>
                   </div>
-                  <span>2 phòng ngủ</span>
+                  <span>{room.phongNgu} phòng ngủ</span>
                   <div className="dot">
                     <i className="fas fa-circle"></i>
                   </div>
-                  <span>2 giường</span>
+                  <span>{room.giuong} giường</span>
                   <div className="dot">
                     <i className="fas fa-circle"></i>
                   </div>
-                  <span>2 phòng tắm</span>
+                  <span>{room.phongTam} phòng tắm</span>
                 </div>
               </div>
               <div className="room-info__heading--right w-20">
@@ -79,35 +111,67 @@ export default function RoomDetail({}: Props) {
             <div className="divider"></div>
             <div className="room-details">
               <div className="room-details__item d-flex">
-                <HouseIcon />
+                <i className="fas fa-home fa-cog stroke-transparent"></i>
                 <div className="room-detail__text">
                   <strong>Toàn bộ nhà</strong>
                   <p>Bạn sẽ có một chung cư cao cấp cho riêng mình</p>
                 </div>
               </div>
               <div className="room-details__item d-flex">
-                <i className="fas fa-home"></i>
+                <i className="fa fa-star fa-cog stroke-transparent"></i>
                 <div className="room-detail__text">
-                  <strong>Toàn bộ nhà</strong>
-                  <p>Bạn sẽ có một chung cư cao cấp cho riêng mình</p>
+                  <strong>Vệ sinh tăng cường</strong>
+                  <p>
+                    Chủ nhà này đã cam kết thực hiện quy trình vệ sinh tăng
+                    cường 5 bước của Airbnb
+                  </p>
                 </div>
               </div>
               <div className="room-details__item d-flex">
-                <i className="fas fa-home"></i>
+                <i className="fas fa-door-open fa-cog stroke-transparent"></i>
                 <div className="room-detail__text">
-                  <strong>Toàn bộ nhà</strong>
-                  <p>Bạn sẽ có một chung cư cao cấp cho riêng mình</p>
-                </div>
-              </div>
-              <div className="room-details__item d-flex">
-                <i className="fas fa-home"></i>
-                <div className="room-detail__text">
-                  <strong>Toàn bộ nhà</strong>
-                  <p>Bạn sẽ có một chung cư cao cấp cho riêng mình</p>
+                  <strong>Tự nhận phòng</strong>
+                  <p>Tự nhận phòng bằng khóa thông minh.</p>
                 </div>
               </div>
             </div>
             <div className="divider"></div>
+            <div className="room-desc">
+              <p>{room.moTa}</p>
+              <NavLink to="" className="">
+                <u>Hiển thị thêm</u>
+              </NavLink>
+            </div>
+            <div className="divider"></div>
+            <div className="room-sleeping-place">
+              <h2>Nơi bạn sẽ ngủ nghỉ</h2>
+              <div className="room-sleeping-place__block">
+                <i className="fas fa-bed stroke-transparent"></i>
+                <div className="room-sleeping-place__block-text">
+                  <strong>Phòng ngủ</strong>
+                  <p>{room.giuong} giường đôi</p>
+                </div>
+              </div>
+            </div>
+            <div className="divider"></div>
+            <div className="room-amenities">
+              <h2>Nơi này có những gì cho bạn</h2>
+              <div className="room-amenities__content row">
+                <div className="room-amenities__content__col col-6">
+                  {renderRoomAmenities(room.hoBoi, "hoBoi")}
+
+                  {renderRoomAmenities(room.bep, "bep")}
+                  {renderRoomAmenities(room.dieuHoa, "dieuHoa")}
+                  {renderRoomAmenities(room.doXe, "doXe")}
+                </div>
+                <div className="room-amenities__content__col col-6">
+                  {renderRoomAmenities(room.banLa, "banLa")}
+                  {renderRoomAmenities(room.mayGiat, "mayGiat")}
+                  {renderRoomAmenities(room.tivi, "tivi")}
+                  {renderRoomAmenities(room.wifi, "wifi")}
+                </div>
+              </div>
+            </div>
           </div>
           {/* room info */}
 
@@ -118,6 +182,11 @@ export default function RoomDetail({}: Props) {
           {/* room booking */}
         </div>
         {/* content */}
+
+        <div className="divider"></div>
+        {/* comment */}
+        <Comment />
+        {/* comment */}
       </div>
     </div>
   );
