7470aca0464fa9b523d61d8974f70d7d54f14550
Duyen Nguyen <dnng1410@gmail.com>
2022-10-27 17:45:00 +0700

    admin dashboard

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import SideBar from "../SideBar/SideBar";
 
-type Props = {};
+type Props = {
+  showSidebar: () => void;
+};
 
-export default function NavBar({}: Props) {
-  const [sidebar, setSidebar] = useState(false);
-  console.log(sidebar);
-
-  const showSidebar = () => setSidebar(!sidebar);
+export default function NavBar({ showSidebar }: Props) {
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <>
-      <div className="navbar">
-        <div className="container py-3">
+      <div className="navbar shadow-sm">
+        <div className="container d-flex justify-content-between align-items-center">
           <NavLink to="#" className="menu-bars" onClick={showSidebar}>
             <i className="fa fa-bars"></i>
           </NavLink>
+          {isLogin ? (
+            <div className="admin-info">
+              <div className="admin-avatar">
+                <img src="https://i.pravatar.cc/200" alt="avatar" />
+                <p>admin'name</p>
+              </div>
+            </div>
+          ) : (
+            <button className="btn--primary">Login</button>
+          )}
         </div>
       </div>
-      <SideBar sidebar={sidebar} setSidebar={setSidebar} />
     </>
   );
 }
--- src/templates/AdminTemplate.tsx
+++ src/templates/AdminTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
 import SideBar from "../components/SideBar/SideBar";
@@ -6,10 +6,39 @@ import SideBar from "../components/SideBar/SideBar";
 type Props = {};
 
 export default function AdminTemplate({}: Props) {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const overlayStyles = {
+    background: "rgba(0, 0, 0, .5)",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  };
+
   return (
     <div>
-      <NavBar />
-      <div className="admin__container bg-secondary w-100">
+      <NavBar showSidebar={showSidebar} />
+      <SideBar sidebar={sidebar} setSidebar={setSidebar} />
+      <div className="container py-4">
+        <div
+          className="overlay"
+          style={
+            sidebar
+              ? {
+                  background: "rgba(0, 0, 0, .5)",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                }
+              : {}
+          }
+        ></div>
         <Outlet />
       </div>
     </div>
